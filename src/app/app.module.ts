651d0ca434d0af3f0ccbbf1193e048c7a2ccf3cc
StefanChirica <stefan.angmaar@gmail.com>
2022-12-05 23:37:36 +0200

    jobs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { JobListingComponent } from './components/job-listing/job-listing.component';
+import { JobDetailsComponent } from './components/job-details/job-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobListingComponent,
+    JobDetailsComponent
   ],
   imports: [
     BrowserModule
