f66bf61055d6714202a746b629c3dd62a5b7ce95
mihail-soltan <mihail.soltan@protonmail.ch>
2023-02-15 21:46:25 +0200

    start search filter

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,18 +1,34 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import data from '../data.json';
+import { Job } from '../job';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  constructor() {}
 
-  constructor() { }
+  titleFilter = new BehaviorSubject(''); //filter by job title, company or expertise
+  locationFilter = new BehaviorSubject('');
+  jobs = new BehaviorSubject<any[]>([]);
 
   getData() {
-    return data;
+    this.jobs.next(data);
+    return this.jobs;
   }
 
   onShowFullTime() {
-    return data.filter((job) => job.contract === 'Full Time')
+    return this.jobs.getValue().filter((job) => job.contract === 'Full Time');
+  }
+
+  onFilter(title: string) {
+    this.getData()
+    const filtered =this.jobs.getValue().filter((job) => job.position.includes(title));
+    this.jobs.next(filtered)
+  }
+
+  setTitleFilter(title: string) {
+    this.titleFilter.next(title);
   }
 }
