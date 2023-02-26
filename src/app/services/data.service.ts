9672ff5f89f85a2055c29d945ad213c6ac7b8ce7
mihail-soltan <mihail.soltan@protonmail.ch>
2023-02-26 21:11:36 +0200

    add getData on filter

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -19,49 +19,17 @@ export class DataService {
     return this.jobs;
   }
 
-  // onShowFullTime() {
-  //   return this.jobs.getValue().filter((job) => job.contract === 'Full Time');
-  // }
-
-  // onFilterByTitle(title: string) {
-  //   this.getData();
-  //   const filtered = this.jobs
-  //     .getValue()
-  //     .filter((job) => job.position.includes(title));
-  //   this.jobs.next(filtered);
-  // }
-
-  // setTitleFilter(title: string) {
-  //   this.titleFilter.next(title);
-  // }
-
-  // onFilterByLocation(location: string) {
-  //   this.getData();
-  //   const filtered = this.jobs
-  //     .getValue()
-  //     .filter((job) => job.location.includes(location));
-  // }
-
   onFilter(title: string = '', location: string = '', fullTime: boolean) {
-    console.log(title);
-    console.log(location);
-    console.log(fullTime);
-    // this.getData()
-    // console.log(this.jobs.value)
-    // if(fullTime){
-    //   const fullTimeOnly = this.jobs.getValue().filter((job) => job.contract === 'Full Time');
-    //   this.jobs.next(fullTimeOnly)
-    // }
-    const filter = this.jobs
-      .getValue()
-      .filter(
-        (job) =>
-          job.position.toLowerCase().includes(title.toLowerCase()) &&
-          job.location.toLowerCase().includes(location.toLowerCase()) &&
-          // (job.contract === 'Full Time' || !fullTime)
-          (fullTime? job.contract === 'Full Time' : job.contract.includes('') )
-      );
-    console.log(filter);
+    this.getData();
+
+    const filter = this.jobs.getValue().filter(
+      (job) =>
+        job.position.toLowerCase().includes(title.toLowerCase()) &&
+        job.location.toLowerCase().includes(location.toLowerCase()) &&
+        // (job.contract === 'Full Time' || !fullTime)
+        (fullTime ? job.contract === 'Full Time' : job.contract.includes(''))
+    );
+
     this.jobs.next(filter);
   }
 }
