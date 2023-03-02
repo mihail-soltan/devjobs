b54429f1eb913f4410faa70a2ef6e8456dd01f4c
mihail-soltan <mihail.soltan@protonmail.ch>
2023-03-02 21:48:31 +0200

    add responsiveness/mobile navbar+search

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, HostListener } from '@angular/core';
 import { SharedService } from 'src/app/services/shared.service';
 import { DataService } from 'src/app/services/data.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -14,12 +14,23 @@ export class SearchComponent implements OnInit {
   locationInput: string = '';
   fullTime: boolean = false;
 
+  searchPlaceholder: string = 'Filter by title, companies, expertise...';
+
+  filterDialogOpen: boolean = false;
+
   constructor(
     private sharedService: SharedService,
     private dataService: DataService,
     private route: ActivatedRoute,
     private router: Router
-  ) {}
+  ) {
+    this.setPlaceholderText();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.setPlaceholderText();
+  }
 
   ngOnInit(): void {
     this.sharedService.fullTime.subscribe((fullTime: boolean) => {
@@ -48,5 +59,19 @@ export class SearchComponent implements OnInit {
     this.locationInput = '';
     this.fullTime = false;
     this.dataService.searching.next(false);
+  }
+
+  setPlaceholderText() {
+    if (window.innerWidth <= 768) {
+      this.searchPlaceholder = 'Filter by title...';
+    } else {
+      this.searchPlaceholder = 'Filter by title, companies, expertise...';
+    }
+  }
+
+
+  onOpenFilterDialog() {
+    this.filterDialogOpen = true;
+    console.log(this.filterDialogOpen);
   }
 }
